d0d4e3487283a22b3eecaa4eee11f6890afda23b
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2025-09-19 11:47:02 +0530

    week-1 - 4.py

---
+++ week-1/4.py
@@ -0,0 +1,29 @@
+# 4. Write a Python program to read and print various types of variables
+"Value: {}, Type: {}"
+
+int_var = 24
+print("Value: {}, Type: {}".format(int_var, type(int_var)))
+
+float_var = 78.96
+print("Value: {}, Type: {}".format(float_var, type(float_var)))
+
+complex_var = 4 + 5j
+print("Value: {}, Type: {}".format(complex_var, type(complex_var)))
+
+list_var = ["Suhani", 24, "Just a photographer"]
+print("Value: {}, Type: {}".format(list_var, type(list_var)))
+
+tuple_var = (22, 67, 78)
+print("Value: {}, Type: {}".format(tuple_var, type(tuple_var)))
+
+string_var = "From fairest creatures we desire increase"
+print("Value: {}, Type: {}".format(string_var, type(string_var)))
+
+dict_var = {"Attitude" : "the way that you think, feel or behave", "Age" : 17}
+print("Value: {}, Type: {}".format(dict_var, type(dict_var)))
+
+set_var = {24, 27, 19, 10}
+print("Value: {}, Type: {}".format(set_var, type(set_var)))
+
+bool_var = True
+print("Value: {}, Type: {}".format(bool_var, type(bool_var)))
